879a1ced0a75d3b088b667f35e8d82d75822d2b7
Skibidi50-lol <realhatroi@gmail.com>
2026-01-20 22:14:19 +0700

    Create Configs.lua

---
+++ index/Configs.lua
@@ -0,0 +1,10 @@
+local CONFIG = {
+    ContainerWidth = 400,
+    ContainerHeight = 110,
+    BarHeight = 6,
+    BarPaddingX = 40,
+    BarLerpSpeed = 0.5,
+    FadeInSpeed = 0.5,
+    FadeOutSpeed = 0.4,
+    ColorTweenSpeed = 0.5
+}
