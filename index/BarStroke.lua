0edfab05f4ce8550d75a752f324175411dee2cec
Skibidi50-lol <realhatroi@gmail.com>
2026-01-20 22:10:37 +0700

    Create BarStroke.lua

---
+++ index/BarStroke.lua
@@ -0,0 +1,6 @@
+self.BarStroke = Instance.new("UIStroke")
+self.BarStroke.Thickness = 1
+self.BarStroke.Color = Color3.fromRGB(45, 45, 48)
+self.BarStroke.Transparency = 1
+self.BarStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+self.BarStroke.Parent = self.BarBackground
