5a409b9aaaefd76b11b531359a9754b637c76f0b
Skibidi50-lol <realhatroi@gmail.com>
2026-01-21 20:02:22 +0700

    Update Configs.lua

--- index/Configs.lua
+++ index/Configs.lua
@@ -4,7 +4,8 @@ local CONFIG = {
     BarHeight = 6,
     BarPaddingX = 40,
     BarLerpSpeed = 0.5,
-    FadeInSpeed = 0.5,
+    FadeInSpeed = 0.6,
     FadeOutSpeed = 0.4,
-    ColorTweenSpeed = 0.5
+    ColorTweenSpeed = 0.5,
+    TypingSpeed = 0.05
 }
